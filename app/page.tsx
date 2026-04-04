7cadcb16b88fc370053d4cfb7f3a65252bc9cb4a
awais <awais.ishtiaq08@gmail.com>
2026-04-04 06:05:20 +0000

    Initial commit: AI SaaS learning companion platform

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button";
+const Page = () => {
+  return (
+    <div>
+      <h1>Welcome TO AI SaaS platform</h1>
+
+    <Button>Share</Button>
+    </div>
+  )
+}
+
+export default Page
